c921769605ab1e1a5f3ad5bdbeb333e627e27d0c
qqmath <124810093+qqmath@users.noreply.github.com>
2025-02-11 14:03:33 -0500

    Delete DEM_generation directory

--- DEM_generation/Updated IMG to DEM pipeline/Pre-processing/coordlist.py
+++
@@ -1,8 +0,0 @@
-def generate_number_pairs(start, end, step, filename):
-    with open(filename, 'w') as file:
-        for i in range(start, end + 1, step):
-            file.write(f"{i}, {i}\n")
-            file.write(f"{i}, {i + 10}\n")
-
-# Example usage
-generate_number_pairs(10, 100000, 10, 'coordlist.txt')
